23b9bc0cdab7b1f67c94f4a292b2cbba5939a46b
BrayanMnz <brayanmnz@hotmail.com>
2021-06-14 00:42:06 -0400

    Gradle, creando vista del formulario que publica las citas

---
+++ app/src/main/java/ProyectoVacunacion/ClienteREST/Controllers/MainController.java
@@ -0,0 +1,35 @@
+package ProyectoVacunacion.ClienteREST.Controllers;
+
+import ProyectoVacunacion.ClienteREST.util.*;
+import io.javalin.Javalin;
+
+
+
+public class MainController extends BaseControlador {
+
+    public MainController(Javalin app) {
+        super(app);
+    }
+
+    @Override
+    public void aplicarRutas() {
+
+        app.get("/", ctx -> {
+
+            ctx.render("/publico/templates/formulario.html");
+        });
+
+        app.get("/create-form", ctx -> {
+            ctx.render("/public/templates/main-form.html");
+        });
+
+        app.get("/list-form", ctx -> {
+            ctx.render("/public/templates/list-form.html");
+        });
+
+
+
+    }
+
+
+}
